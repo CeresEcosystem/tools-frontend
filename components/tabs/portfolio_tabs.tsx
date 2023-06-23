5acdd76cb3d1eb890b32e66ddacd5cf9b9c86070
Srdjan Djukanovic <116005505+sdjuka@users.noreply.github.com>
2023-06-23 18:44:06 +0200

    add portfolio staking, rewards and liquidity

---
+++ components/tabs/portfolio_tabs.tsx
@@ -0,0 +1,42 @@
+import { PortfolioTab } from '@interfaces/index';
+import classNames from 'classnames';
+
+export default function PortfolioTabs({
+  tabs,
+  selectedTab,
+  changeSelectedTab,
+}: {
+  tabs: PortfolioTab[];
+  selectedTab: PortfolioTab;
+  // eslint-disable-next-line no-unused-vars
+  changeSelectedTab: (tab: PortfolioTab) => void;
+}) {
+  return (
+    <div className="max-w-lg mb-10 px-5 py-2 mx-auto w-full bg-backgroundItem flex rounded-xl">
+      {tabs.map((tab) => {
+        const selected = tab.tab === selectedTab.tab;
+
+        return (
+          <div
+            key={tab.tab}
+            onClick={() => changeSelectedTab(tab)}
+            className={classNames(
+              'flex-1 flex justify-center items-center rounded-xl py-2 cursor-pointer group',
+              selected && 'bg-white bg-opacity-10'
+            )}
+          >
+            <span
+              className={classNames(
+                'text-sm text-white capitalize font-medium md:text-base',
+                selected ? 'text-opacity-100' : 'text-opacity-50',
+                'group-hover:text-opacity-100'
+              )}
+            >
+              {tab.tab}
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
--- hooks/use_portfolio.ts
+++ hooks/use_portfolio.ts
@@ -1,62 +1,179 @@
-import { NEW_API_URL } from '@constants/index';
 import { usePolkadot } from '@context/polkadot_context';
-import { PortfolioItem } from '@interfaces/index';
+import { useEffect, useState, useCallback, ChangeEvent, useRef } from 'react';
+import usePersistState from './use_persist_state';
+import {
+  PortfolioItem,
+  PortfolioLiquidityItem,
+  PortfolioStakingRewardsItem,
+  PortfolioTab,
+} from '@interfaces/index';
+import { NEW_API_URL } from '@constants/index';
 import { getEncodedAddress } from '@utils/helpers';
-import { useEffect, useRef, useState } from 'react';
+
+export type LoadingStatus =
+  | 'Loading'
+  | 'No extension'
+  | 'Not connected'
+  | 'Connected';
+
+const tabs = [
+  { tab: 'Portfolio', permalink: '/portfolio/' },
+  { tab: 'Staking', permalink: '/portfolio/staking/' },
+  { tab: 'Rewards', permalink: '/portfolio/rewards/' },
+  { tab: 'Liquidity', permalink: '/portfolio/liquidity/' },
+];
 
 const usePortfolio = () => {
   const polkadot = usePolkadot();
 
+  const [loadingStatus, setLoadingStatus] = useState<LoadingStatus>('Loading');
+  const [selectedTab, setSelectedTab] = useState(tabs[0]);
+  const [inputWalletAddress, setInputWalletAddress] = usePersistState(
+    '',
+    'WALLET_ADDRESS'
+  );
   const [loading, setLoading] = useState(false);
 
-  const [portfolioItems, setPortfolioItems] = useState<
-    PortfolioItem[] | string
-  >('');
+  const portfolioItems = useRef<
+    | (PortfolioItem | PortfolioStakingRewardsItem | PortfolioLiquidityItem)[]
+    | undefined
+  >();
 
   const totalValue = useRef(0);
 
-  useEffect(() => {
-    if (polkadot?.selectedAccount?.address) {
-      setLoading(true);
+  function sortAndFilterItems(
+    array: (
+      | PortfolioItem
+      | PortfolioStakingRewardsItem
+      | PortfolioLiquidityItem
+    )[]
+  ) {
+    const itemsFiltered = array.filter((pi) => {
+      if ('balance' in pi) {
+        return pi.balance >= 0.0001 && pi.value >= 0.0001;
+      }
 
-      fetch(
-        `${NEW_API_URL}/portfolio/${getEncodedAddress(
-          polkadot?.selectedAccount?.address
-        )}`
-      )
-        .then(async (response) => {
-          const json = (await response.json()) as PortfolioItem[];
+      return pi.value >= 0.0001;
+    });
 
-          const itemsFiltered = json.filter((pi) => pi.balance >= 0.0001);
+    itemsFiltered.sort(
+      (portfolioItem1, portfolioItem2) =>
+        portfolioItem2.value - portfolioItem1.value
+    );
+    let tv = 0;
+    itemsFiltered.map((dataItem) => {
+      tv += dataItem.value;
+    });
+    totalValue.current = tv;
+    portfolioItems.current = itemsFiltered;
 
-          itemsFiltered.sort(
-            (portfolioItem1, portfolioItem2) =>
-              portfolioItem2.value - portfolioItem1.value
+    setLoading(false);
+  }
+
+  const fetchPortfolioItems = useCallback(
+    async (address: string) => {
+      if (!loading) {
+        setLoading(true);
+      }
+
+      if (address !== '') {
+        try {
+          const response = await fetch(
+            `${NEW_API_URL}${selectedTab.permalink}${getEncodedAddress(
+              address
+            )}`
           );
+          const json = await response.json();
 
-          let tv = 0;
+          if (json && json?.length > 0) {
+            let array = [];
 
-          itemsFiltered.map((dataItem) => {
-            tv += dataItem.value;
-          });
-
-          totalValue.current = tv;
-
-          setPortfolioItems(itemsFiltered);
+            if (selectedTab.tab === 'Portfolio') {
+              array = json as PortfolioItem[];
+              sortAndFilterItems(array);
+            } else if (
+              selectedTab.tab === 'Staking' ||
+              selectedTab.tab === 'Rewards'
+            ) {
+              array = json as PortfolioStakingRewardsItem[];
+              sortAndFilterItems(array);
+            } else {
+              array = json as PortfolioLiquidityItem[];
+              sortAndFilterItems(array);
+            }
+          } else {
+            portfolioItems.current = [];
+            totalValue.current = 0;
+            setLoading(false);
+          }
+        } catch {
+          portfolioItems.current = [];
+          totalValue.current = 0;
           setLoading(false);
-        })
-        .catch(() => {
-          setPortfolioItems([]);
-          setLoading(false);
-        });
+        }
+      } else {
+        portfolioItems.current = undefined;
+        totalValue.current = 0;
+        setLoading(false);
+      }
+    },
+    [selectedTab.permalink, selectedTab.tab, loading]
+  );
+
+  const handleWalletAddressChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setInputWalletAddress(e.target.value);
+    },
+    [setInputWalletAddress]
+  );
+
+  const changeSelectedTab = (tab: PortfolioTab) => {
+    setLoading(true);
+    setSelectedTab(tab);
+  };
+
+  useEffect(() => {
+    if (!polkadot?.loading) {
+      if (polkadot?.accounts && polkadot?.accounts?.length > 0) {
+        if (polkadot?.selectedAccount?.address) {
+          setLoadingStatus('Connected');
+        } else {
+          setLoadingStatus('Not connected');
+        }
+      } else {
+        setLoadingStatus('No extension');
+      }
+    }
+  }, [
+    polkadot?.accounts,
+    polkadot?.loading,
+    polkadot?.selectedAccount?.address,
+  ]);
+
+  useEffect(() => {
+    if (loadingStatus === 'Connected') {
+      fetchPortfolioItems(polkadot?.selectedAccount?.address!);
+    } else if (loadingStatus === 'No extension' && inputWalletAddress !== '') {
+      fetchPortfolioItems(inputWalletAddress);
     } else {
-      totalValue.current = 0;
-      setPortfolioItems('Waiting for wallet connection...');
       setLoading(false);
     }
-  }, [polkadot?.selectedAccount?.address]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [loadingStatus, selectedTab]);
 
-  return { loading, portfolioItems, totalValue: totalValue.current };
+  return {
+    loadingStatus,
+    tabs,
+    selectedTab,
+    changeSelectedTab,
+    inputWalletAddress,
+    handleWalletAddressChange,
+    walletAddress: polkadot?.selectedAccount?.address,
+    portfolioItems: portfolioItems.current,
+    loading,
+    totalValue: totalValue.current,
+    fetchPortfolioItems,
+  };
 };
 
 export default usePortfolio;
